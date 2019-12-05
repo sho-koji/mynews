c4293c0f8786176e7d2bad8e2328da86c3b01d12
sho-koji <kojisho717@gmail.com>
2019-12-05 07:59:08 +0000

    submission of assingment no.11

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -5,11 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
     </head>
     <body>
         <h1>Myニュース作成画面</h1>
-        
+    
         {{-- layouts/admin.blade.phpを読み込む --}}
         @extends('layouts.admin')
         
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,23 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>MyNews</title>
     </head>
+    
     <body>
-        <h1>クリエイト画面</h1>
+            
+    @extends('layouts.profile')
+    
+    @section('title', 'プロフィール作成')
+    
+    @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィール作成画面</h2>
+                </div>
+            </div>
+        </div>
+    @endsection
+    
     </body>
 </html>
