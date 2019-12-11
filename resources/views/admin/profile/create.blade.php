5badca6c6f18bd6c276d46c68b6f8a0a9b1ed673
sho-koji <kojisho717@gmail.com>
2019-12-11 12:31:38 +0000

    submission of assignment no.12 revised

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-    </head>
-    
-    <body>
-            
-    @extends('layouts.profile')
-    
-    @section('title', 'プロフィール作成')
-    
-    @section('content')
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 mx-auto">
-                    <h2>プロフィール作成画面</h2>
-                </div>
+@extends('layouts.profile')
+
+@section('title', 'プロフィール作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール作成画面</h2>
             </div>
         </div>
-    @endsection
-    
-    </body>
-</html>
+    </div>
+@endsection
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>MyNews</title>
-    </head>
+@extends('layouts.profile')
+
+@section('title', 'プロフィール編集')
+
+@section('content')
     <body>
         <h1>エディット画面</h1>
     </body>
-</html>
